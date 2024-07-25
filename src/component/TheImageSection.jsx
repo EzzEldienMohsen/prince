535b74f6366dd2da37bde92f4732cc3c14371b83
Ezz dghedy <ezzmohsend@gmail.com>
2024-07-25 21:07:23 +0200

    the image card is done

--- src/component/TheImageSection.jsx
+++ src/component/TheImageSection.jsx
@@ -3,17 +3,12 @@ import layout from '../assets/svg/mainPage/layout.svg';
 
 const TheImageSection = () => {
   return (
-    <div className="w-full py-16">
-      <div className="relative w-full gap-y-10 md:gap-y-0 md:h-[350px] lg:h-[500px] md:w-3/5 bg-theRed pt-8 md:py-8 flex flex-col justify-center items-center md:flex-row md:justify-start">
-        {/* Content will be here */}
-        <h1 className="text-white font-tajawal md:text-start w-4/5 px-8 lg:px-16 font-semiBold text-2xl md:text-3xl lg:text-4xl text-center">
-          {' '}
-          للشركة حضوراً فعالاً في كافة المعارض والمؤتمرات الخاصة بالبناء
-        </h1>
-        <div className="md:absolute flex justify-center md:justify-normal  md:top-1/2 md:transform md:-translate-y-1/2 md:translate-x-[-75%] ">
-          <img src={layout} alt="alt" className="w-4/5 -mb-8 md:w-4/5 md:mb-0 " />
-        </div>
-      </div>
+    <div className="flex flex-col my-16  justify-center  items-center md:flex-row md:justify-between w-full relative gap-y-8 md:gap-y-0 pt-8 md:py-8 ">
+      <div className="absolute top-0 right-0 w-full md:w-11/12 lg:w-3/4 h-full bg-theRed -z-10 "></div>
+      <h1 className="text-white text-xl px-8 lg:px-16 md:text-3xl lg:text-4xl font-tajawal font-extraBold text-center md:text-start">
+        للشركة حضوراً فعالاً في كافة المعارض والمؤتمرات الخاصة بالبناء
+      </h1>
+      <img src={layout} alt="layout" className="w-4/5 -mb-10 md:mb-0 md:w-1/2" />
     </div>
   );
 };
