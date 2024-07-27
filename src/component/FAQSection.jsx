943d3f5a8b75243a5fec6a726a9dcb972089bb7e
Ezz dghedy <ezzmohsend@gmail.com>
2024-07-27 19:12:42 +0200

    faq section translation and request is done

--- src/component/FAQSection.jsx
+++ src/component/FAQSection.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import { FAQsComponent } from '../subComponent';
+import { useTranslation } from 'react-i18next';
 
 const FAQSection = () => {
+  const {t} =useTranslation()
   return (
     <div className="w-full flex bg-theLGray flex-col justify-center items-center px-8 lg:px-16 my-16 py-10 gap-y-6 md:gap-y-8 lg:gap-y-12 text-center">
       <p className="font-tajawal font-light text-theRed text-lg md:text-2xl">
         FAQs
       </p>
       <h1 className="font-bold font-tajawal text-black text-xl md:text-3xl">
-        الأسألة المتكررة
+        {t('faqs')}
       </h1>
-      <FAQsComponent/>
+      <FAQsComponent />
     </div>
   );
 }
--- src/subComponent/FAQsComponent.jsx
+++ src/subComponent/FAQsComponent.jsx
@@ -1,30 +1,40 @@
 import React from 'react'
 import { theFAQ } from '../assets';
+import { useGlobalData } from '../context/GlobalDataContext';
 
 const FAQsComponent = () => {
+  const { data, isLoading } = useGlobalData();
+
+
+   if (isLoading) {
+     return (
+       <div className="w-full aspect-square flex justify-center items-center">
+         <span className="loading loading-spinner text-theRed loading-xl"></span>
+       </div>
+     );
+   }
   return (
-    <div className='flex flex-col justify-start items-start w-full '>
-        {theFAQ.map((faq)=>{
-            return (
-              <div
-              key={faq.id}
-                tabIndex={0}
-                className="collapse collapse-arrow border-base-300 text-start border border-l-0 border-r-0 rounded-none bg-theLGray"
-              >
-                <div className="collapse-title text-xl font-medium">
-                  {faq.text}
-                </div>
-                <div className="collapse-content">
-                  <p>
-                    tabindex={0} attribute is necessary to make the div
-                    focusable
-                  </p>
-                </div>
-              </div>
-            );
-        })}
+    <div className="flex flex-col justify-start items-start w-full ">
+      {data?.faqs?.map((faq) => {
+        return (
+          <div
+            key={faq.id}
+            tabIndex={0}
+            className="collapse collapse-arrow border-base-300 text-start border border-l-0 border-r-0 rounded-none bg-theLGray"
+          >
+            <div className="collapse-title text-xl font-medium">
+              {faq.question}
+            </div>
+            <div className="collapse-content">
+              <p>
+                {faq.answer}
+              </p>
+            </div>
+          </div>
+        );
+      })}
     </div>
-  )
+  );
 }
 
 export default FAQsComponent
