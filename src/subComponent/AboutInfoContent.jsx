bcfd258ba4d4cade8003d2372af8ad9c63cad321
Ezz dghedy <ezzmohsend@gmail.com>
2024-07-28 14:16:26 +0200

    about page is done as a ui, translation, separating and re using code , and api requests adj

--- src/subComponent/AboutInfoContent.jsx
+++ src/subComponent/AboutInfoContent.jsx
@@ -8,16 +8,20 @@ const AboutInfoContent = ({data}) => {
   return (
     <div className="w-full lg:w-1/2 flex flex-col items-start justify-center mt-4 lg:mt-0 gap-y-4">
       <div className="w-full flex flex-col justify-center items-start gap-y-4 ">
-        <p className="font-tajawal text-theRed text-2xl">{t('about')}</p>
-        <h1 className="text-black font-tajawal font-medium text-2xl md:text-3xl lg:text-2xl">
-          {t('mainPageTitle')}
+        <p className="font-tajawal text-theRed text-2xl">
+          {t('aboutSecondSectionTitle')}
+        </p>
+        <h1 className="text-black font-tajawal font-medium text-2xl md:text-3xl ">
+          {t('aboutSecondSectionBigTitle')}
         </h1>
 
         <p className="text-[#7C8893] font-tajawal font-normal md:text-sm">
           {t('aboutAboutText')}
         </p>
         <p
-          className={`text-[#7C8893] font-tajawal font-normal md:text-sm ${isArabic?"mr-4":"ml-4"} `}
+          className={`text-[#7C8893] font-tajawal font-normal md:text-sm ${
+            isArabic ? 'mr-4' : 'ml-4'
+          } `}
           dangerouslySetInnerHTML={{ __html: data.content }}
         />
       </div>
