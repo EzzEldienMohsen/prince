368201bbdd65740a9d4ba10a2ae20adad0e1c82f
Ezz dghedy <ezzmohsend@gmail.com>
2024-07-29 19:25:07 +0200

    hero section is updated

--- src/subComponent/MainHeroInfo.jsx
+++ src/subComponent/MainHeroInfo.jsx
@@ -5,7 +5,7 @@ import { useTranslation } from 'react-i18next';
 const MainHeroInfo = () => {
   const {t} = useTranslation()
   return (
-    <div className="w-full mt-6 lg:mt-0 lg:w-2/5 flex flex-col justify-center items-start gap-y-2 md:gap-y-4 lg:gap-y-6 ">
+    <div className="w-full mt-6 lg:mt-0 lg:w-1/2 flex flex-col justify-center items-start gap-y-2 md:gap-y-4 lg:gap-y-6 ">
       <p className="font-tajawal text-theRed">{t('mainPageHeroFirstLine')}</p>
       <h1 className="text-theFontColor font-tajawal font-extraBold text-2xl md:text-2xl lg:text-4xl ">
         {t('mainPageTitle')}
@@ -14,7 +14,7 @@ const MainHeroInfo = () => {
         {t('mainPageHeroSecondRedLine')}
       </h3>
       <p className="text-[#7C8893] w-full font-tajawal font-normal md:text-xl">
-        {t('mainPageHeroTextAndOthers')}{' '}
+        {t('mainPageHeroTextAndOthers')}
       </p>
       <MainButtons />
     </div>
