b6709a943c9938d0104fa60da637473ebe4772ab
Ezz dghedy <ezzmohsend@gmail.com>
2024-07-29 21:28:24 +0200

    modal card update

--- src/subSubComponent/TheModalBody.jsx
+++ src/subSubComponent/TheModalBody.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import ImageWithFallback from './ImageWithFallBack';
-
+import { useGlobalContext } from './../context/GlobalContext';
+import close from "../assets/svg/projectCard/close.svg"
 const TheModalBody = ({ theData, link, slideId, nextSlideId, prevSlideId }) => {
+  const {isArabic} = useGlobalContext()
   return (
     <dialog
       id={link.id}
-      className="modal w-full bg-transparent flex justify-center items-center"
+      className="modal px-8 lg:px-16 w-full bg-transparent flex justify-center items-center"
     >
       <div className="flex flex-col justify-center items-start gap-y-3 w-4/5 ">
-        <form method="dialog">
-          <button className="btn btn-sm btn-circle btn-ghost">✕</button>
+        <form method="dialog" className=" w-full">
+          <button className={`btn btn-sm btn-circle btn-ghost absolute top-5 ${isArabic?"left-5 md:left-10":"right-5 md:right-10"} `}>
+          <img src={close} alt="close" />
+          </button>
         </form>
         <div className="carousel w-full">
           <div
@@ -19,7 +23,7 @@ const TheModalBody = ({ theData, link, slideId, nextSlideId, prevSlideId }) => {
             <ImageWithFallback
               src={link.image}
               alt={link.title}
-              className="w-[271.66px] h-[218.22px] md:w-[480.63px] md:h-[386.08px] lg:w-[741.65px] lg:h-[595] aspect-auto"
+              className=" aspect-auto"
             />{' '}
             <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
               <a href={`#${prevSlideId}`} className="btn btn-circle">
