7ddef8c6657a72ac7808b7869564f682fde87e37
Ezz dghedy <ezzmohsend@gmail.com>
2024-07-31 15:43:40 +0200

    adj

--- src/subSubComponent/ImageWithFallBack.jsx
+++ src/subSubComponent/ImageWithFallBack.jsx
@@ -2,25 +2,35 @@ import React from 'react';
 import errorGuard from '../assets/svg/errorGuard/error.svg';
 
 const ImageWithFallback = ({ src, alt, className }) => {
-  
   const [imgSrc, setImgSrc] = React.useState(src);
-  const [theClass,setTheClass] = React.useState(className)
+  const [theClass, setTheClass] = React.useState(className);
   React.useEffect(() => {
     if (!src) {
       setImgSrc(errorGuard);
       setTheClass('bg-[#DADADA] flex justify-center items-center');
     }
   }, [src]);
-  return (
-    <div className="flex w-full justify-center items-center bg-[#DADADA] h-[214px]">
+  if (src) {
+    return (
       <img
         src={imgSrc}
         alt={alt}
         className={theClass}
         onError={() => setImgSrc(errorGuard)}
       />
-    </div>
-  );
-};
+    );
+  } else {
+    return (
+      <div className="flex w-full justify-center items-center bg-[#DADADA] h-[214px]">
+        <img
+          src={imgSrc}
+          alt={alt}
+          className={theClass}
+          onError={() => setImgSrc(errorGuard)}
+        />
+      </div>
+    );
+  }
+}
 
 export default ImageWithFallback;
